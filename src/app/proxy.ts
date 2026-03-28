e8173cd4e643760879abe3eaab4ead8aba4e1775
Abeer Mourtaja <abeermourtaja@gmail.com>
2026-03-28 16:07:31 +0300

    update proxy

--- src/app/proxy.ts
+++ src/app/proxy.ts
@@ -2,7 +2,7 @@ import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(req:NextRequest){
-    const token=await getToken({req})
+    const token=await getToken({req,secret:process.env.NEXTAUTH_SECRET})
     if(!!token)
     return NextResponse.next()  
     return NextResponse.redirect(new URL('/login',req.url))
